0b38c86fd3e99b68b98e7653763b27151a29520b
Semih Orhan <sorhan3.14@gmail.com>
2023-07-12 17:34:29 +0300

    create header parser endpoint

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,11 +19,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(8000, function () {
+  console.log('Your app is listening on port 8000');
 });
